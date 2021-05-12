134644031f6e843cb562dfd9c5cd5d6ba1d8efea
simonallen <simonallen19930901@gmail.com>
2021-05-12 15:54:24 +0800

    Add second page

---
+++ src/pages/build/index.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { useSelector, connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import Content from "components/Content";
+import { addCount } from "store/count/action";
+import { wrapper } from "store/store";
+import { serverRenderClock, startClock } from "store/tick/action";
+import Link from "next/link";
+
+const build = (props) => {
+  const state = useSelector((state) => state);
+
+  useEffect(() => {
+    const timer = props.startClock();
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [props]);
+
+  return (
+    <>
+      <Content title="build Page" />
+      <Link href="/build/123">
+        <a>build/123</a>
+      </Link>
+    </>
+  );
+};
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async ({ store }) => {
+    store.dispatch(serverRenderClock(true));
+    store.dispatch(addCount());
+  }
+);
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addCount: bindActionCreators(addCount, dispatch),
+    startClock: bindActionCreators(startClock, dispatch),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(build);
